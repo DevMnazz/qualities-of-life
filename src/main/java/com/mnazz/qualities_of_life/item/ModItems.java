f787f060af467e1e954f5b412b46f12f576cac55
Mnazz <120224610+DevMnazz@users.noreply.github.com>
2025-12-20 00:02:01 -0500

    Add Interdimensional Gem advancements and tooltips

--- src/main/java/com/mnazz/qualities_of_life/item/ModItems.java
+++ src/main/java/com/mnazz/qualities_of_life/item/ModItems.java
@@ -1,12 +1,19 @@
 package com.mnazz.qualities_of_life.item;
 
 import com.mnazz.qualities_of_life.Qualities_of_life;
+import net.minecraft.ChatFormatting;
 import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+
+import java.util.List;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
@@ -14,7 +21,20 @@ public class ModItems {
 
 
     public static final RegistryObject<Item> INTERGEM = ITEMS.register("intergem",
-            () -> new Item(new Item.Properties().fireResistant()));
+            () -> new Item(new Item.Properties().fireResistant()) {
+        @Override
+                public boolean isFoil(ItemStack stack) {
+                    return true;
+        }
+                public void appendHoverText(ItemStack stack, Level level, List<Component> tooltipComponents, TooltipFlag isAdvanced) {
+                    tooltipComponents.add(Component.translatable("tooltip.qualities_of_life.intergem. line1")
+                            .withStyle(ChatFormatting.AQUA));
+                    tooltipComponents.add(Component.translatable("tooltip.qualities_of_life.intergem.line2")
+                            . withStyle(ChatFormatting. GRAY, ChatFormatting.ITALIC));
+
+                    super. appendHoverText(stack, level, tooltipComponents, isAdvanced);
+                }
+            });
 
 
     public static void register(IEventBus eventBus) {
